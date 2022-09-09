726461f3df1be563f774155087410a590d972ede
julu1994 <mrjewel837@gmail.com>
2022-09-09 03:17:48 +0200

    designing user messages

--- src/Components/Chat/chat.js
+++ src/Components/Chat/chat.js
@@ -1,8 +1,25 @@
 import "./chat.scss";
 import React from "react";
+import Messages from "../Messages/messages";
+import { Avatar } from "@mui/material";
 
 const Chat = () => {
-    return <div className="chat">Chat</div>;
+    return (
+        <div className="chat">
+            <div className="chat-messages">
+                <Avatar alt="Jewel" src="/static/images/avatar/1.jpg" />
+                <Messages />
+            </div>
+            <div className="chat-messages">
+                <Avatar alt="Jewel" src="/static/images/avatar/1.jpg" />
+                <Messages />
+            </div>
+            <div className="chat-messages">
+                <Avatar alt="Jewel" src="/static/images/avatar/1.jpg" />
+                <Messages />
+            </div>
+        </div>
+    );
 };
 
 export default Chat;
---
+++ src/Components/Messages/messages.js
@@ -0,0 +1,17 @@
+import "./messages.scss";
+import React from "react";
+
+const Messages = () => {
+    return (
+        <div className="messages">
+            <p className="messages-text">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat.
+            </p>
+        </div>
+    );
+};
+
+export default Messages;
