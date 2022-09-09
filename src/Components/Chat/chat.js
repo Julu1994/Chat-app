458f221a11bbbfc0f652872e93e80f551e4f1923
julu1994 <mrjewel837@gmail.com>
2022-09-09 04:44:38 +0200

    designed the inputs and responsive layout

--- src/Components/Chat/chat.js
+++ src/Components/Chat/chat.js
@@ -1,24 +1,58 @@
 import "./chat.scss";
 import React from "react";
 import Messages from "../Messages/messages";
-import { Avatar } from "@mui/material";
+import {
+    Avatar,
+    Button,
+    createTheme,
+    IconButton,
+    ThemeProvider,
+} from "@mui/material";
+import SendIcon from "@mui/icons-material/Send";
+import PhotoCamera from "@mui/icons-material/PhotoCamera";
 
 const Chat = () => {
+    const theme = createTheme({
+        palette: {
+            primary: {
+                main: "#003366",
+            },
+        },
+    });
     return (
-        <div className="chat">
-            <div className="chat-messages">
-                <Avatar alt="Jewel" src="/static/images/avatar/1.jpg" />
-                <Messages />
+        <ThemeProvider theme={theme}>
+            <div className="chat">
+                <div className="chat-messages">
+                    <Avatar alt="Jewel" src="/static/images/avatar/1.jpg" />
+                    <Messages />
+                </div>
+                <div className="chat-messages">
+                    <Avatar alt="Jewel" src="/static/images/avatar/1.jpg" />
+                    <Messages />
+                </div>
+                <div className="chat-messages">
+                    <Avatar alt="Jewel" src="/static/images/avatar/1.jpg" />
+                    <Messages />
+                </div>
+                <div className="chat-input">
+                    <input type="text" placeholder="Write here" />
+                    <IconButton
+                        color="primary"
+                        aria-label="upload picture"
+                        component="label">
+                        <input hidden accept="image/*" type="file" />
+                        <PhotoCamera />
+                    </IconButton>
+                    <Button
+                        variant="contained"
+                        color="primary"
+                        size="small"
+                        endIcon={<SendIcon />}>
+                        Send
+                    </Button>
+                </div>
             </div>
-            <div className="chat-messages">
-                <Avatar alt="Jewel" src="/static/images/avatar/1.jpg" />
-                <Messages />
-            </div>
-            <div className="chat-messages">
-                <Avatar alt="Jewel" src="/static/images/avatar/1.jpg" />
-                <Messages />
-            </div>
-        </div>
+        </ThemeProvider>
     );
 };
 
--- src/Components/Messages/messages.js
+++ src/Components/Messages/messages.js
@@ -4,12 +4,7 @@ import React from "react";
 const Messages = () => {
     return (
         <div className="messages">
-            <p className="messages-text">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat.
-            </p>
+            <p className="messages-text">Lorem ipsum dolor sit amet,</p>
         </div>
     );
 };
