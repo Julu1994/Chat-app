6b88b7eb47b86deae131012eedafcd0541e0cd41
julu1994 <mrjewel837@gmail.com>
2022-09-13 14:29:19 +0200

    working on creating pairing two users for chat

--- src/Components/ChatList/chatList.js
+++ src/Components/ChatList/chatList.js
@@ -1,9 +1,16 @@
 import "./chatList.scss";
 import React from "react";
-import { useToggle } from "../../context";
+import { useFireauth, useToggle } from "../../context";
 import Search from "./search";
 import { Avatar } from "@mui/material";
-import { collection, getDocs, query, where } from "firebase/firestore";
+import {
+    collection,
+    doc,
+    getDoc,
+    getDocs,
+    query,
+    where,
+} from "firebase/firestore";
 import { database } from "../../Firebase/auth";
 import toast from "react-hot-toast";
 
@@ -11,6 +18,7 @@ const ChatList = () => {
     const toggle = useToggle();
     const [findUser, setFindUser] = React.useState("");
     const [user, setUser] = React.useState(null);
+    const activeUser = useFireauth();
     const handleChage = (event) => {
         setFindUser(event.target.value);
     };
@@ -23,23 +31,41 @@ const ChatList = () => {
             querySnapshot.forEach((doc) => {
                 setUser(doc.data());
             });
-            if (!user) {
-                toast("User not found");
-            }
         } catch {
             toast.error("Something went wrong!");
+        }
+        if (!user) {
+            toast("User not found");
         }
     };
     const handleKey = (event) => {
         event.code === "Enter" && handleSearch();
     };
-    console.log(user, "the user for search ");
+
+    const handleConnect = async () => {
+        const userIds =
+            activeUser.uid > user.id
+                ? activeUser.uid + user.id
+                : user.id + activeUser.uid;
+        try {
+            const docRef = doc(database, "messages", userIds);
+            const userChat = await getDoc(docRef);
+            if (userChat.exists()) {
+                console.log("Document data:", userChat.data());
+            } else {
+                // doc.data() will be undefined in this case
+                console.log("No such document!");
+            }
+        } catch {
+            toast.error("Something went wrong!!");
+        }
+    };
 
     return (
         <div className={toggle ? "list" : "hideList"}>
             <Search onchange={handleChage} onkeydown={handleKey} />
             {user && (
-                <div className="user">
+                <div className="user" onClick={handleConnect}>
                     <Avatar alt="Jewel" src="/static/images/avatar/1.jpg" />
                     <div className="user-info">
                         <p className="user-info-name">{user.name}</p>
